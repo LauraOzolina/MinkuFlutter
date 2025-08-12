624fd5e954a86c40f117b01ac1a1a6300d991dbc
LauraOzolina <laura.ozolina.latvia@gmail.com>
2025-08-12 16:49:59 +0300

    updated app icon and set fullscreen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Permission.camera.request();
-  await Permission.microphone.request();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();//initialize Flutter engine, binding setup
+  await Permission.camera.request();//Pieprasa piekļuvi kamerai
+  runApp(const MyApp());//Startē lietotni
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 }
 
 class MyApp extends StatelessWidget {
@@ -21,23 +22,23 @@ class MyApp extends StatelessWidget {
 
 class WebViewPage extends StatefulWidget {
   @override
-  State<WebViewPage> createState() => _WebViewPageState();
+  State<WebViewPage> createState() => _WebViewPageState();//WebView izveide, kur tiek parādīts weblapas saturs
 }
 
 class _WebViewPageState extends State<WebViewPage> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {//izveido aplikācijas skatu, kur ielādē web lapu
     return Scaffold(
-      appBar: AppBar(title: const Text("Minku")),
       body: InAppWebView(
         initialUrlRequest: URLRequest(
-          url: WebUri("https://viavrarlab.github.io/MinkuFinal/"),
+          url: WebUri("https://viavrarlab.github.io/MinkuFinal/"),//ielādē WEBlapu
         ),
         initialSettings: InAppWebViewSettings(
           mediaPlaybackRequiresUserGesture: false,
           allowsInlineMediaPlayback: true,
         ),
         onPermissionRequest: (controller, request) async {
+          //piešķir atļauju, ja tāda ir dota
           return PermissionResponse(
             resources: request.resources,
             action: PermissionResponseAction.GRANT,
@@ -48,68 +49,4 @@ class _WebViewPageState extends State<WebViewPage> {
   }
 }
 
-/*void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minku',
-      theme: ThemeData(
-        primarySwatch: Colors.purple
-      ),
-      home: const WebViewDemo(),
-    );
-  }
-}
-
-class WebViewDemo extends StatefulWidget {
-  const WebViewDemo({super.key});
-
-  @override
-  State<WebViewDemo> createState() => _WebViewDemoState();
-}
-
-class _WebViewDemoState extends State<WebViewDemo> {
-  List<CameraDescription> cameras = [];
-  CameraController? cameraController;
-  WebViewController controller = WebViewController(
-    onPermissionRequest: (WebViewPermissionRequest request) => request.grant(),
-  );
-
-  @override
-  void initState(){
-    super.initState();
-    requestPermissions();
-    //_setupCameraController();
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(NavigationDelegate(
-        onProgress: (progress) => {},
-        onPageStarted: (url) => {},
-        onPageFinished:  (url) => {},
-      ))
-      ..loadRequest(Uri.parse('https://viavrarlab.github.io/MinkuFinal/'));
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Minku')
-      ),
-      body: WebViewWidget(controller: controller),
-    );
-  }
-
-  Future<void> requestPermissions() async {
-    await [
-      Permission.camera,
-    ].request();
-  }
-}*/
 
